ad2a96bd471893a36ef1ac0af345624d00293a37
FullStackSimon <fullstacksimon@gmail.com>
2024-09-13 13:17:02 +0200

    finished nav

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Link from "next/link";
 import { Button } from "./ui/button";
 import DesktopNavigation from "./DesktopNavigation";
+import MobileNav from "./MobileNav";
 
 const Header = () => {
   return (
